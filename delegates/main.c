40a15651ce6c3d251d796b105c8ff12acea5e436
qHomeee <wanttodie@gmail.com>
2026-09-24 16:41:31 +0300

    i need to continue delegates

--- delegates/main.c
+++ delegates/main.c
@@ -5,9 +5,22 @@
 #include "stdlib.h"
 
 
-int* return_two_cores(int a, int b, int c){
-    int *result[2] = {0,0};
-    return *result;
+
+int*  my_type_addition(int a, int b, int c){
+    if(c == 0){
+        int* result = calloc(1, sizeof(int));
+        *result = 0;
+        return result;
+    }
+    int *result = (a+b)/c;
+    return result;
+}
+
+int* my_substraction(int a, int b, int c){
+    int* result = calloc(2, sizeof(int));
+    result[0] = a;
+    result[1] = b-c;
+    return result;
 }
 
 
@@ -15,8 +28,46 @@ int main()
 {
     typedef int* (*Operation)(int, int, int);
 
-    Operation x = return_two_cores;
-    printf("%d", x(1,2,3));
+    Operation x = my_substraction;
+    int* y = x(5,4,2);
+    for (int i = 0; i < 2; i++)
+    {
+        printf("%d\n", y[i]);
+    }
+    
+
+    typedef Operation Operations[3];
+    Operations arr;
+    arr[0] = my_substraction;
+    arr[1] = my_type_addition;
+    arr[2] = my_substraction;
+
+    for (int i = 0; i < 3; i++)
+    {
+        int* y;
+        switch(i){
+            case 0:
+                y =  arr[i];
+                for (int j = 0; j < 2; j++)
+                {
+                    printf("%d\n", y[j]);
+                }
+                continue;
+            case 1:
+                y = arr[i];
+                printf("%d\n",y);
+                continue;
+            case 2:
+                y =  arr[i];
+                for (int j = 0; j < 2; j++)
+                {
+                    printf("%d\n", y[j]);
+                }
+                continue;
+                
+        }
+    }
+    
     
 
 }
